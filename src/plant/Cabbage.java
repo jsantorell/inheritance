34a23166feee470bced62457a02dee42f5df12f0
jerem <jerem@192.168.88.1>
2017-02-08 15:23:29 -0600

    Plant HW 1.3

---
+++ src/plant/Cabbage.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package plant;
+
+/**
+ *
+ * @author jerem
+ */
+public class Cabbage extends LeafyGreen {
+
+    private double price;
+
+    public Cabbage(double price, int months, String a) {//cabbage
+        super(months, a);
+        this.price = price;
+        System.out.print("Price: ");
+        System.out.println(price);
+    }
+
+    public double getShape() {
+        return price;
+    }
+
+    public void setShape(double shape) {
+        this.price = shape;
+    }
+
+    
+
+}
